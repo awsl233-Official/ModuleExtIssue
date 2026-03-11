5650acb8c4c233f3a1c4bed86ffeac491e1f4513
awsl233 <unconfigured@null.spigotmc.org>
2026-03-11 23:43:13 +0800

    smth

--- src/main/java/dev/illusion/features/Module.java
+++ src/main/java/dev/illusion/features/Module.java
@@ -2,4 +2,11 @@ package dev.illusion.features;
 
 //empty but enough to reproduce the issue
 public class Module {
+
+
+
+    //this fails too
+    public void test(){
+        staticMethod();
+    }
 }
